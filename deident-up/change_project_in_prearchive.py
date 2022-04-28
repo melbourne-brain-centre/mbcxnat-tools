8c628fa384845aa9e0b1b7d4bbfc6f54f083b801
birendrarokaha <birenrokaha1@gmail.com>
2022-04-28 10:34:18 +1000

    change paroject in prearchive update

--- deident-up/change_project_in_prearchive.py
+++ deident-up/change_project_in_prearchive.py
@@ -2,25 +2,52 @@
 @birendra
 Change project of xnat sessions form prearchive
 '''
-
 import xnat as x
 import os
+import sys
 import argparse
 from getpass import getpass
+
+
+def check_projects(session, new_proj):
+    # Check if user provided project is available
+    try:
+        temp = session.projects[new_proj]
+        print(f"Source project : {new_proj} available!")
+    except:
+        print(f"Source project : {new_proj} not found!\nTry again.")
+        print(f"Only these projects available to current user are:")
+        for i, each in enumerate(session.projects):
+            print(f"\t{i+1}. {each}")
+        print(f"Select one of them.")
+        sys.exit()
+
 
 def connect_xnat(xnat_site,uname,pword,new_proj):
     # Connect with xnat instance
     with x.connect(xnat_site, user=uname, password=pword) as session:
+        check_projects(session, new_proj)
         transfer_success, transfer_fail = 0,0
         print(f"Succesfully connected to {xnat_site}")
         # Select all sessions in prearchive
         prearc = session.prearchive.sessions()
+
+        # Check if there are prearchive sessions
+        if len(prearc) == 0:
+            print("No sessions in prearchive.")
+            sys.exit()
+
+        # Loop through prearchive
         for eachp in prearc:
             work_subject = eachp.subject
             print(f"Working on {work_subject}")
+
+            # Generate URL links
             org_link = eachp.external_uri()
             temp = org_link.split("/")[4:]
             pre_arc_src = f"/{('/').join(temp)}"
+
+            # Send the  POST request
             try:
                 session.post(path=f"{xnat_site}data/services/prearchive/move", data={"src":pre_arc_src,"newProject":new_proj})
                 print(f"Moved : {work_subject} to project : {new_proj}")
@@ -54,9 +81,11 @@ def main():
     pword =  getpass("Enter your XNAT password: ")
     xnat_site = args.xnat_server
 
+    # Url format check
     if not xnat_site[-1] == "/":
         xnat_site = xnat_site + "/"
     
+    # Conect to xnat server
     print(f"Connecting to {xnat_site} as {uname}")
     ts, tf = connect_xnat(xnat_site,uname,pword,new_proj)
     print(f"Transfer Success : {ts}\nTransfer Fail : {tf}")
